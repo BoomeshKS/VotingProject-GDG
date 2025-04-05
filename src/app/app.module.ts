06267198093b89587c63911b0b05545228c56987
BoomeshKS <boomeshalone@gmail.com>
2025-04-05 13:21:59 +0530

    dashboard added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideRouter, RouterModule } from '@angular/router';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
+
 @NgModule({
-  imports: [BrowserModule, RouterModule],
-  providers: [
-    provideRouter([
-      { path: '', component: HomeComponent },
-      { path: 'login', component: LoginComponent },
-      { path: 'dashboard', component: DashboardComponent }
-    ]),
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    DashboardComponent
   ],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
